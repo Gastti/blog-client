b654ae19d589827c55bd9d476fcdd86825075e48
gastti <gutigas96@gmail.com>
2023-08-09 17:35:46 -0300

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ProtectedRoute } from './routes/ProtectedRoute.tsx'
+import { Dashboard } from './routes/Dashboard.tsx'
+import { AuthProvider } from './auth/AuthProvider.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Dashboard />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>,
+)
