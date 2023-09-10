60cea093eaf3319227d8aad5c4ad0e31e93466c6
Gastti <gutigas96@gmail.com>
2023-09-10 13:21:58 -0300

    add posts with PostFullView component

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,17 +1,21 @@
 import './Home.css'
 import { useEffect, useState } from 'react'
 import Container from '../../components/Container/Container'
-import { Posts } from '../../types'
+import { Post } from '../../types'
 import axios from 'axios'
+import PostFullView from '../../components/Posts/FullView/PostFullView'
 
 export default function Search() {
-  const [posts, setPosts] = useState<Array<Posts>>([])
+  const [posts, setPosts] = useState<Array<Post>>([])
 
   const getPosts = async () => {
     const API_URL = 'http://localhost:3000/api/v1'
     const API_ENDPOINT = '/posts'
     axios.get(`${API_URL}${API_ENDPOINT}`)
-    .then(response => setPosts(response.data.data))
+    .then(response => {
+      setPosts(response.data.data)
+      console.log(response)
+    })
     .catch(error => console.log(error))
   }
 
@@ -22,34 +26,7 @@ export default function Search() {
   return (
     <Container className='home'>
       {posts.map(post => (
-        <article className='article' key={post._id}>
-          <div className='article-author'>
-            <div className='author-avatar'>
-              <img src={post.author.avatar}/>
-            </div>
-            <div>
-              <p className='author-fullname'>
-                {post.author.firstname} {post.author.lastname}
-              </p>
-              <p className='author-username'>
-                @{post.author.username}
-              </p>
-            </div>
-          </div>
-          <div className="article-header">
-            <span>Publicado el 10 de Septiembre, 2023</span>
-            <h2>{post.title}</h2>
-          </div>
-          <div className='article-cover'>
-            <img src={post.image}/>
-          </div>
-          <div className='article-tags'>
-            {post.tags.map(tag => <span>{tag}</span>)}
-          </div>
-          <div className='article-content'>
-            {post.content}
-          </div>
-        </article>
+        <PostFullView post={post}/>
       ))}
     </Container>
   )
