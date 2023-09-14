805d419ffbb8e0c6f0f9b57ac5fb21409e33c58d
gastti <gutigas96@gmail.com>
2023-09-14 18:05:27 -0300

    add posts services

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,18 +2,16 @@ import './Home.css'
 import { useEffect, useState } from 'react'
 import Container from '../../components/Container/Container'
 import { Post } from '../../types'
-import axios from 'axios'
 import PostQuickView from '../../components/Posts/QuickView/PostQuickView'
 import Loader from '../../components/Loader/Loader'
+import { getAllPosts } from '../../services/posts'
 
 export default function Search() {
   const [posts, setPosts] = useState<Array<Post>>([])
   const [isLoading, setIsLoading] = useState<boolean>(true)
 
   const getPosts = async () => {
-    const API_URL = 'https://blog-api-nqc2.onrender.com/api/v1'
-    const API_ENDPOINT = '/posts'
-    axios.get(`${API_URL}${API_ENDPOINT}`)
+    getAllPosts()
       .then(response => {
         setPosts(response.data.data)
         setIsLoading(false)
