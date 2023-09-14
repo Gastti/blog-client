556d66aa4a9cb85d26486acb0593f5449bc82e3a
gastti <gutigas96@gmail.com>
2023-09-14 18:05:03 -0300

    add Login Page and change Register Page logic

---
+++ src/pages/Auth/Login/LoginPage.tsx
@@ -0,0 +1,12 @@
+
+import './LoginPage.css'
+import Login from '../../../components/Login/Login'
+import Container from '../../../components/Container/Container'
+
+export default function LoginPage() {
+  return (
+    <Container className='register-container'>
+      <Login />
+    </Container>
+  )
+}
--- src/pages/Auth/Register/RegisterPage.tsx
+++ src/pages/Auth/Register/RegisterPage.tsx
@@ -1,9 +1,10 @@
+import './RegisterPage.css'
 import Register from '../../../components/Register/Register'
 import Container from '../../../components/Container/Container'
 
 export default function RegisterPage() {
   return (
-    <Container>
+    <Container className='register-container'>
       <Register />
     </Container>
   )
