a16a041a05c5c985914e1812b7e7159439ebbe1f
gastti <gutigas96@gmail.com>
2023-09-14 18:01:40 -0300

    edit loader style

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import './Loader.css'
 export default function Loader() {
   return (
     <div className='loader-container'>
-      <div className='loader'></div>
+      <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
     </div>
   )
 }
