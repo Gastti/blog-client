e6c20d287c1c4530901579625aab9e407bcddc4f
gastti <gutigas96@gmail.com>
2023-09-14 18:06:39 -0300

    add types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,33 +1,54 @@
 export interface Post {
-  _id:       string;
-  author:    Author;
-  title:     string;
-  content:   string;
-  category:  string;
-  image:     Image;
-  tags:      string[];
-  url:       string;
+  _id: string;
+  author: Author;
+  title: string;
+  content: string;
+  category: string;
+  image: Image;
+  tags: string[];
+  url: string;
   createdAt: Date;
 }
 
 export interface Author {
-  username:  string;
+  username: string;
   firstname: string;
-  lastname:  string;
-  avatar:    string;
-  role:      string;
+  lastname: string;
+  avatar: string;
+  role: string;
 }
 
 export interface Image {
-  _id:            string;
-  url:            string;
+  _id: string;
+  url: string;
   expirationDate: string;
 }
 
-export interface RegisterEntry {
-  username:  string
+export interface RegisterValues {
+  username: string
   firstname: string
-  lastname:  string
-  email:     string
-  password:  string
+  lastname: string
+  email: string
+  password: string
+}
+
+export interface LoginValues {
+  email: string
+  password: string
+}
+
+export interface User {
+  username: string
+  firstname: string
+  lastname: string
+  email: string
+  avatar: string
+  contactUrl: string
+  role: string
+  biography: string
+}
+
+export interface Tokens {
+  access: string;
+  refresh: string;
 }
