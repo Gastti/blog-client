3049ce07a6f4f8bbfe300f960242309bf80f0c29
gastti <gutigas96@gmail.com>
2023-09-19 22:45:51 -0300

    remove unused components

--- src/utils/handleTokens.ts
+++
@@ -1,4 +0,0 @@
-export function handleTokens (accessToken: string, refreshToken: string) {
-  localStorage.setItem("accessToken", accessToken)
-  localStorage.setItem("refreshToken", refreshToken)
-}
