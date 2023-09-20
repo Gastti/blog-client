0095019841d2d771d959f278c6c67e5b2aab7937
gastti <gutigas96@gmail.com>
2023-09-19 22:50:02 -0300

    general changes

--- src/pages/Posts/Edit/EditPost.tsx
+++ src/pages/Posts/Edit/EditPost.tsx
@@ -1,42 +1,44 @@
 import './EditPost.css'
 import 'react-quill/dist/quill.snow.css'
-import Editor from '../../../components/Editor/Editor'
 import Container from '../../../components/Container/Container'
-import { methods } from '../../../enums'
-import { useLocation } from 'react-router-dom'
-import { getPostById } from '../../../services/posts'
-import { useEffect, useState } from 'react'
+// import { methods } from '../../../enums'
+// import { useLocation } from 'react-router-dom'
+// import { getPostById } from '../../../services/posts'
+// import { useEffect, useState } from 'react'
+// import { PostEntry } from '../../../types'
 
 export default function EditPost() {
-  const [post, setPost] = useState({})
-  const { search } = useLocation()
+  // const [post, setPost] = useState<PostEntry>({})
+  // const { search } = useLocation()
 
-  const getPost = async (query: string) => {
-    const id = query.split('=')[1]
+  // const getPost = async (query: string) => {
+  //   const id = query.split('=')[1]
 
-    getPostById(id)
-      .then(response => {
-        const { post } = response.data
-        const postValues = {
-          title: post.title,
-          category: post.category,
-          tags: post.tags.join(','),
-          content: post.content
-        }
-        setPost(postValues)
-      })
-      .catch(error => console.log(error))
-  }
+  //   getPostById(id)
+  //     .then(response => {
+  //       const { post } = response.data
+  //       const postValues = {
+  //         title: post.title,
+  //         category: post.category,
+  //         tags: post.tags.join(','),
+  //         content: post.content
+  //       }
+  //       setPost(postValues)
+  //     })
+  //     .catch(error => console.log(error))
+  // }
 
-  useEffect(() => {
-    if (search !== null && search !== undefined) {
-      getPost(search)
-    }
-  }, [search])
+  // console.log(post)
+
+  // useEffect(() => {
+  //   if (search !== null && search !== undefined) {
+  //     getPost(search)
+  //   }
+  // }, [search])
 
   return (
     <Container className='create-post-container'>
-      <Editor method={methods.PUT} />
+      {/* <Editor method={methods.PUT} post={post} /> */}
     </Container>
   )
 }
