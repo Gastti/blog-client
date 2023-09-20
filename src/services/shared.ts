8cf791b30fce4bfb31984514f1b3bd4c3cd84bdb
gastti <gutigas96@gmail.com>
2023-09-19 22:46:51 -0300

    add routes

---
+++ src/services/shared.ts
@@ -0,0 +1,3 @@
+export const baseUrl = 'http://localhost:3000/api/v1'
+export const baseUrls = 'https://blog-api-nqc2.onrender.com/api/v1'
+export const baseUrlss = 'https://s61tz4kq-3000.brs.devtunnels.ms/api/v1'
