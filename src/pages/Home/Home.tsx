c3e980922151125da627c8580706616f7f358a9d
gastti <gutigas96@gmail.com>
2023-09-19 22:51:02 -0300

    remove console log

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,7 +15,6 @@ export default function Search() {
       .then(response => {
         setPosts(response.data.data)
         setIsLoading(false)
-        console.log('Home, getAllPost response:', response)
       })
       .catch(error => console.log(error))
   }
