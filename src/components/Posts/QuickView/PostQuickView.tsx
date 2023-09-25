451a40994de93b0b631201bf0fc9098d662a4a70
Gastti <gutigas96@gmail.com>
2023-09-25 18:13:45 -0300

    edit post views

--- src/components/Posts/QuickView/PostQuickView.tsx
+++ src/components/Posts/QuickView/PostQuickView.tsx
@@ -1,13 +1,14 @@
 import './PostQuickView.css'
 import { Post } from '../../../types'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import DOMPurify from 'dompurify'
-import { format, parseISO, isToday } from 'date-fns';
+import { parseISO, formatDistance } from 'date-fns';
 import { es } from 'date-fns/locale';
 import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined'
 
 export default function PostQuickView({ post }: { post: Post }) {
   const cleanPostContent = DOMPurify.sanitize(post.content)
+  const navigate = useNavigate()
 
   const splitText = (text: string, maxLength: number) => {
     if (text.length > maxLength) {
@@ -28,18 +29,18 @@ export default function PostQuickView({ post }: { post: Post }) {
   }
 
   const parsedDate = parseISO(post.createdAt.toString());
-  const publishedToday = isToday(parsedDate)
-  const formatedDate = format(parsedDate, "dd 'de' MMMM 'del' yyyy", { locale: es });
-  const publishAt = publishedToday ? 'Hoy' : formatedDate
+  const currentDate = new Date()
+  const publishAt = formatDistance(currentDate, parsedDate, {
+    addSuffix: false,
+    includeSeconds: true,
+    locale: es
+  });
 
-  const splitedContent = splitText(cleanPostContent, 400) as string
+
+  const splitedContent = splitText(cleanPostContent, 200) as string
 
   return (
     <article className='post-quick-view'>
-      <div className='pqv-cover'>
-        <img src={post.image.url} />
-      </div>
-
       <div className='pqv-author'>
         <div className='pqv-author-info'>
           <div className='avatar'>
@@ -50,17 +51,31 @@ export default function PostQuickView({ post }: { post: Post }) {
           </p>
         </div>
       </div>
-      <div className="pqv-header">
-        <h2 className='title'><Link to={`/read?post=${post._id}`}>{post.title}</Link></h2>
+
+      <div className='pqv-cover'>
+        <img src={post.image.url} />
       </div>
 
-      <div className='pqv-content' dangerouslySetInnerHTML={{ __html: splitedContent + '...' }}>
+      {/* <div className="pqv-header">
+        <h2 className='title'><Link to={`/read?post=${post._id}`}>{post.title}</Link></h2>
+      </div> */}
+
+      <div 
+      className='pqv-content-container'
+      onClick={() => navigate(`/read?post=${post._id}`)}>
+        <h2 className='pqv-title'>{post.title}</h2>
+        <div
+          className='pqv-content'
+          dangerouslySetInnerHTML={{ __html: splitedContent }}
+        />
+        <div className='pqv-footer'>
+          <span className='pqv-publishat'>Hace {publishAt}.</span>
+          <Link className='btn-readpost' to={`/read?post=${post._id}`}>
+            Leer
+            <KeyboardArrowRightOutlinedIcon />
+          </Link>
+        </div>
       </div>
-      <span className='pqv-publishat'>Publicado el {publishAt}</span>
-      <Link className='btn-readpost' to={`/read?post=${post._id}`}>
-        Leer
-        <KeyboardArrowRightOutlinedIcon />
-      </Link>
     </article>
   )
 }
